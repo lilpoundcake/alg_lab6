850e2fa1b0a4a3a92a5f51d0e9af79002db9ffe6
lilpoundcake <44047612+lilpoundcake@users.noreply.github.com>
2019-04-17 10:34:02 +0700

    Не дописал код

--- stack.py
+++ stack.py
@@ -12,14 +12,36 @@ class StackIsEmptyError(RuntimeError):
 
 class Stack:
     def __init__(self, size):
+        self.size = size
         self.storage = [0] * size
         self.head = -1
 
     def push(self, v):
-        pass
+        if self.head == self.size - 1:
+            raise StackOverflowError()
+        elif self.head < self.size - 1:
+            self.head += 1
+            self.storage[self.head] = v
 
     def pop(self):
-        pass
+        if self.head == -1:
+            raise StackIsEmptyError()
+        else:
+            return self.storage[self.head]
+            self.storage[self.head] = 0
+            self.head -= 1
 
     def __len__(self):
-        pass
+        return self.head + 1
+
+"""stack = Stack(1)
+print(stack.__len__())
+stack.push(12)
+try:
+    stack.push(12)
+except StackOverflowError:
+    print('StackOverflowError')
+print(stack.__len__())
+stack.pop()
+stack.pop()
+print(stack.__len__())"""
